19668bc86237195b95655044ab26eb802fc0d70d
akulOn <dracaluka12@gmail.com>
2022-07-29 10:53:07 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+var client = new HttpClient();
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+var requests = Enumerable
+    .Range(1, 1000)
+    .Select(x => Task.Run(async () =>
+    {
+        var response = await client.GetAsync("http://www.google.com");
+        var responseContent = await response.Content.ReadAsStringAsync();
+    }));
+
+await Task.WhenAll(requests);
+
+Console.WriteLine($"Zahtevi su gotovi za: {stopwatch.Elapsed.TotalMilliseconds}");
